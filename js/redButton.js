f4e55156ea6a3a9efe3455143942e66399b4b980
vaibhavbhoir <vaibhavbhoir99@gmail.com>
2021-07-19 20:19:24 +0530

    initial commit

---
+++ js/redButton.js
@@ -0,0 +1,11 @@
+function red(){
+    document.getElementById("bg").style.display="block";
+    document.getElementById("bg").style.backgroundSize="cover";
+    document.getElementById("bg").src = "https://cdn.wallpapersafari.com/92/42/BDvT3C.jpg";
+
+
+    document.getElementById("noWorry").innerHTML ="Do not worry JK";
+   
+    document.getElementById("noWorry").style.fontSize ="25px";
+  
+}
